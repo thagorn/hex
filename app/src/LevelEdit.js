4539c28b0a97ed67d0ddbcd1757cc95ab271d11f
thagorn <thagorn1@gmail.com>
2023-09-03 18:26:32 -0400

    Add proxy, initial routing, basic object editing

---
+++ app/src/LevelEdit.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Button, Container, Form, FormGroup, Input, Label } from 'reactstrap';
+import AppNavbar from './AppNavbar';
+
+const LevelEdit = () => {
+    const initialFormState = {
+        name: '',
+        description: '',
+    };
+    const [level, setLevel] = useState(initialFormState);
+    const navigate = useNavigate();
+    const { id } = useParams();
+
+    useEffect(() => {
+        if (id !== 'new') {
+            fetch(`/api/level/${id}`)
+                .then(response => response.json())
+                .then(data => setLevel(data));
+        }
+    }, [id, setLevel]);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target
+
+        setLevel({ ...level, [name]: value })
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        await fetch(`/api/level${level.id ? `/${level.id}` : '/'}`, {
+            method: (level.id) ? 'PUT' : 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(level)
+        });
+        setLevel(initialFormState);
+        navigate('/levels');
+    }
+
+    const title = <h2>{level.id ? 'Edit Level' : 'Create Level'}</h2>;
+
+    return (<div>
+            <AppNavbar/>
+            <Container>
+                {title}
+                <Form onSubmit={handleSubmit}>
+                    <FormGroup>
+                        <Label for="name">Name</Label>
+                        <Input type="text" name="name" id="name" value={level.name || ''}
+                               onChange={handleChange}/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="description">Description</Label>
+                        <Input type="text" name="description" id="description" value={level.description || ''}
+                               onChange={handleChange}/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Button color="primary" type="submit">Save</Button>
+                        <Button color="secondary" tag={Link} to="/levels">Cancel</Button>
+                    </FormGroup>
+                </Form>
+            </Container>
+        </div>
+    )
+};
+
+export default LevelEdit;
---
+++ app/src/LevelList.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import { Button, ButtonGroup, Container, Table } from 'reactstrap';
+import AppNavbar from './AppNavbar';
+import { Link } from 'react-router-dom';
+
+const LevelList = () => {
+
+    const [levels, setLevels] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+
+        fetch('api/level/')
+            .then(response => response.json())
+            .then(data => {
+                setLevels(data);
+                setLoading(false);
+            })
+    }, []);
+
+    const remove = async (id) => {
+        await fetch(`/api/level/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then(() => {
+            let updatedLevels = [...levels].filter(i => i.id !== id);
+            setLevels(updatedLevels);
+        });
+    }
+
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+
+    const levelList = levels.map(level => {
+        return <tr key={level.id}>
+            <td style={{whiteSpace: 'nowrap'}}>{level.name}</td>
+            <td>{level.description}</td>
+            <td>
+                <ButtonGroup>
+                    <Button size="sm" color="primary" tag={Link} to={"/levels/" + level.id}>Edit</Button>
+                    <Button size="sm" color="danger" onClick={() => remove(level.id)}>Delete</Button>
+                </ButtonGroup>
+            </td>
+        </tr>
+    });
+
+    return (
+        <div>
+            <AppNavbar/>
+            <Container fluid>
+                <div className="float-end">
+                    <Button color="success" tag={Link} to="/levels/new">Add Level</Button>
+                </div>
+                <h3>My Level</h3>
+                <Table className="mt-4">
+                    <thead>
+                    <tr>
+                        <th width="20%">Name</th>
+                        <th width="20%">Description</th>
+                        <th width="10%">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {levelList}
+                    </tbody>
+                </Table>
+            </Container>
+        </div>
+    );
+};
+
+export default LevelList;
