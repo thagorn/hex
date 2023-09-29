0d5f1ec22261adb4f2316fc22a1d8254466be5a2
thagorn <thagorn1@gmail.com>
2023-09-28 20:18:25 -0400

    make it compile and 302 (#7)

--- src/main/java/com/thagorn/hex/hexserver/auth/WebSecurityConfig.java
+++ src/main/java/com/thagorn/hex/hexserver/auth/WebSecurityConfig.java
@@ -15,41 +15,31 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests()
-                .antMatchers("/public/**").permitAll() // Allow access to public resources
-                .anyRequest().authenticated() // Require authentication for all other requests
-            .and()
-            .formLogin()
-                .loginPage("/login") // Customize the login page URL
-                .loginProcessingUrl("/api/perform_login")
-                .defaultSuccessURL("/levels") // Redirect after successful login
-                .failureUrl("/login?error=true")
-                .permitAll()
-            .and()
-            .logout()
-                .logoutSuccessUrl("/login?logout") // Redirect after logout
-                .permitAll();
-
+            .authorizeHttpRequests((requests) -> requests
+                    .requestMatchers("/", "/home").permitAll()
+                    .anyRequest().authenticated()
+                    )
+            .formLogin((form) -> form
+                    .loginPage("/login")
+                    .loginProcessingUrl("/api/perform_login")
+                    .permitAll()
+                    )
+            .logout((logout) -> logout.permitAll());
         return http.build();
     }
 
-	@Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+             User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
 
-		return new InMemoryUserDetailsManager(user);
-	}
-
-    @Bean 
-    public PasswordEncoder passwordEncoder() { 
-    return new BCryptPasswordEncoder(); 
-}
+        return new InMemoryUserDetailsManager(user);
+    }
 }
