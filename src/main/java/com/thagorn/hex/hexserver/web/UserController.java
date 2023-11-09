24fb46dfa8f5e44624676a5b087661219872d76d
Paul Goodman <paul.goodman@gmail.com>
2023-11-09 15:12:15 -0500

    Add get route to UserController

--- src/main/java/com/thagorn/hex/hexserver/web/UserController.java
+++ src/main/java/com/thagorn/hex/hexserver/web/UserController.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 
 import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/api/user")
@@ -32,6 +35,11 @@ public class UserController {
         User result = userRepository.save(user);
         return ResponseEntity.created(new URI("/api/user/" + result.getId()))
                 .body(result);
+    }
+
+    @GetMapping("/")
+    Collection<User> getAll() {
+        return userRepository.findAll();
     }
 
 }
